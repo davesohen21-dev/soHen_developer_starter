fe5812d562a45ca5e6a3abc95167cac1ddc07fe5
soHen Dave <dave@nolische.com>
2026-01-14 15:25:51 +0200

    Update solution

--- 4_setup_apps.bat
+++ 4_setup_apps.bat
@@ -1,1 +1,16 @@
+if not exist "c:\development\nolische\_clients\sohen_project_zukiskitchen" (
+  git clone git@github.com:soHen-Research/sohen_project_zukiskitchen.git "c:\development\nolische\_clients\sohen_project_zukiskitchen"
+)
+
+
+if not exist "c:\development\nolische\_clients\sohen_project_tswalang" (
+  git clone git@github.com:soHen-Research/sohen_project_tswalang.git "c:\development\nolische\_clients\sohen_project_tswalang"
+)
+
+
+if not exist "c:\development\nolische\_clients\sohen_project_heritage" (
+  git clone git@github.com:soHen-Research/sohen_project_heritage.git "c:\development\nolische\_clients\sohen_project_heritage"
+)
+
+echo "Create symbolic links"
 runas /user:localadmin "powershell.exe -Command Start-Process c:\soHen_developer_starter\admin_scripts\setup_apps.bat -Verb RunAs"
--- admin_scripts/setup_apps.bat
+++ admin_scripts/setup_apps.bat
@@ -1,21 +1,13 @@
 echo "Setup nolische clients"
-if not exist "c:\development\nolische\_clients\sohen_project_zukiskitchen" (
-  git clone git@github.com:soHen-Research/sohen_project_zukiskitchen.git "c:\development\nolische\_clients\sohen_project_zukiskitchen"
-)
+
 if not exist "c:\development\nolische\_clients\sohen_project_zukiskitchen\node_modules" (
   mklink /d "c:\development\nolische\_clients\sohen_project_zukiskitchen\node_modules" "c:\node\node_10_dependency_repo_generic\node_modules"
 )
 
-
-if not exist "c:\development\nolische\_clients\sohen_project_tswalang" (
-  git clone git@github.com:soHen-Research/sohen_project_tswalang.git "c:\development\nolische\_clients\sohen_project_tswalang"
-)
 if not exist "c:\development\nolische\_clients\sohen_project_tswalang\node_modules" (
   mklink /d "c:\development\nolische\_clients\sohen_project_tswalang\node_modules" "c:\node\node_10_dependency_repo_generic\node_modules"
 )
 
-
-
-if not exist "c:\development\nolische\_clients\sohen_project_heritage" (
-  git clone git@github.com:soHen-Research/sohen_project_heritage.git "c:\development\nolische\_clients\sohen_project_heritage"
+if not exist "c:\development\nolische\_clients\sohen_project_heritage\node_modules" (
+  mklink /d "c:\development\nolische\_clients\sohen_project_heritage\node_modules" "c:\node\node_10_dependency_repo_generic\node_modules"
 )
