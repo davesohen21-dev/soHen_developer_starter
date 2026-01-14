2143d0940f743570ae4ee25002b4f6c528ac9f5b
soHen Dave <dave@nolische.com>
2026-01-14 15:46:58 +0200

    Update solution

--- 3_setup_xampp.bat
+++ 3_setup_xampp.bat
@@ -9,3 +9,7 @@ if not exist "C:\development\local_hosting_htdocs" (
 if not exist "C:\development\local_web_dev_sites" (
   git clone git@github.com:soHen-Research/local_web_dev_sites.git "C:\development\local_web_dev_sites"
 )
+
+
+echo "Create symbolic links"
+runas /user:localadmin "powershell.exe -Command Start-Process c:\soHen_developer_starter\admin_scripts\configure_xamp_symbolic_links.bat -Verb RunAs"
---
+++ admin_scripts/configure_xamp_symbolic_links.bat
@@ -0,0 +1,7 @@
+del C:\xampp\apache\conf\extra\httpd-vhosts.conf
+mklink C:\xampp\apache\conf\extra\httpd-vhosts.conf C:\development\local_web_dev_sites\apache_config\httpd-vhosts.conf
+
+del C:\xampp\apache\conf\httpd.conf
+mklink C:\xampp\apache\conf\httpd.conf C:\development\local_web_dev_sites\apache_config\httpd.conf
+
+mklink /D C:\xampp\local_sites C:\development\local_web_dev_sites\local_sites
