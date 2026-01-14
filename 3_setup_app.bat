9cb483bb1be4fcb4844e05b199ea484ef3be4fdc
soHen Dave <dave@nolische.com>
2026-01-14 13:53:20 +0200

    Update solution

--- 3_setup_app.bat
+++ 3_setup_app.bat
@@ -1,30 +1,3 @@
-mkdir c:\node
-mkdir c:\development\nolische\_base
-mkdir c:\development\nolische\_branches
-mkdir c:\development\nolische\_clients
-mkdir c:\development\nolische\_family
-mkdir c:\development\nolische\_subsidiaries
-mkdir c:\development\work
-mkdir c:\development\local_hosting_htdocs
-
-
-
-echo "Start with Node"
-if not exist "C:/node/node_10_dependency_repo_generic" (
-  git clone git@github.com:soHen-Research/node_10_dependency_repo_generic.git "C:/node/node_10_dependency_repo_generic"
-)
-
-if not exist "C:/node/node_10_dependency_repo_generic/node_modules" (
-  cd C:/node/node_10_dependency_repo_generic&&npm install
-)
-
-if not exist "C:/node/node_10_dependency_repo_b5c" (
-  git clone git@github.com:soHen-Research/node_10_dependency_repo_b5c.git "C:/node/node_10_dependency_repo_b5c"
-)
-
-if not exist "C:/node/node_10_dependency_repo_b5c/node_modules" (
-  cd C:/node/node_10_dependency_repo_b5c&&npm install
-)
 
 
 echo "Xampp support"
