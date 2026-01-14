fde5454e4d93c10922c20ec459b591d771315d56
soHen Dave <dave@nolische.com>
2026-01-14 17:08:04 +0200

    Update solution

--- admin_scripts/setup_apps.bat
+++ admin_scripts/setup_apps.bat
@@ -2,11 +2,11 @@ echo "Setup nolische clients"
 
 
 if not exist "c:\development\nolische\_projects\gs_enterprise_launch_pad\node_modules" (
-  mklink /d "c:\development\nolische\_projects\gs_enterprise_launch_pad\node_modules" "c:\node\node_10_dependency_repo_generic\node_modules"
+  mklink /d "c:\development\nolische\_projects\gs_enterprise_launch_pad\node_modules" "c:\node\node_10_dependency_repo\node_modules"
 )
 
 if not exist "c:\development\nolische\_projects\open_demo\node_modules" (
-  mklink /d "c:\development\nolische\_projects\open_demo\node_modules" "c:\node\node_10_dependency_repo_generic\node_modules"
+  mklink /d "c:\development\nolische\_projects\open_demo\node_modules" "c:\node\node_10_dependency_repo_b5c\node_modules"
 )
 
 
